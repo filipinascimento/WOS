7ebd24db89f241bcf8e5e75b0e31cf5815d1c860
Filipi Silva <filipinascimento@gmail.com>
2021-06-17 20:14:20 -0400

    Updated to 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='WOS', 
-      version='0.1', 
+      version='0.2', 
       description = 'Database to store and process WOS ', 
       author = 'Filipi N. Silva', 
       packages = ['WOS']
